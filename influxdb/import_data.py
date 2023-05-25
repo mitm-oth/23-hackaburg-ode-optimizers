0aeadb231598f6187d6be548a67f77aa514e792e
Cynog <thomasratzketr@outlook.de>
2023-05-25 21:49:40 +0000

    import data in smaller chunks worked

--- influxdb/import_data.py
+++ influxdb/import_data.py
@@ -1,29 +1,25 @@
+#import required packages
 import pandas as pd
-import datetime as dt
-
 import influxdb_client
-from influxdb_client import InfluxDBClient, Point, WritePrecision
 from influxdb_client.client.write_api import SYNCHRONOUS
 
+# influxdb config
 token = "4twszgEIHmgV0NfWQOkLHpIp-XfQoMXgiBELM5RlTWCdedWxTaX0ktqkfruSORou0s8vdYedkrNQqV2lSVqDVw=="
 org = "isd"
 url = "http://localhost:8086"
 bucket="isd"
 
+# get client and write api
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-
-df = pd.read_parquet("data")
+# read data into pandas df       
+df = pd.read_parquet("../data")
 df["rtctime"] = pd.to_datetime(df["rtctime"], unit="ms")
-colnames = df.columns
+df.set_index("rtctime", inplace=True)
 
-for i, row in df.iterrows():
-    point = Point("sensor")
-    for c in colnames[1:]:
-        point = point.field(c, row[c])
-    point.time(row["rtctime"], WritePrecision.MS)
-    
-    write_api.write(bucket=bucket, org=org, record=point)
-    
-    print(i)
+# write data in small chunks
+step = 10000
+for i in range(0, len(df), step):
+    print(f"\rprogress: {i/len(df): .2f}", end="")
+    write_api.write(bucket, record=df.iloc[i:i+step], data_frame_measurement_name="sensors")
