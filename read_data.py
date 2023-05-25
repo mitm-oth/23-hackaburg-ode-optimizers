39f52f4e7482fcfd81d2c84d368b75743df8d887
Cynog <thomasratzketr@outlook.de>
2023-05-25 22:47:46 +0000

    convert unix timestamps and set as index

--- read_data.py
+++ read_data.py
@@ -8,6 +8,8 @@ DATAPATH = "data"
 # function to read the data into a pandas dataframe
 def read_data(skip=None):
     df = pd.read_parquet(DATAPATH)
+    df["rtctime"] = pd.to_datetime(df["rtctime"], unit="ms")
+    df = df.set_index("rtctime")
 
     if skip is not None:
         df = df.iloc[0:len(df):skip]
@@ -17,5 +19,6 @@ def read_data(skip=None):
 
 # test function read_data()
 if __name__ == "__main__":
-    df = read_data(skip=1000)
+    df = read_data(skip=100)
     print(df.shape)
+    print(df.head())
