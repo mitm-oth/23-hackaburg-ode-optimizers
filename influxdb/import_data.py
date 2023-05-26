a3d00a5515819a61dc48cb9eb7f7ed81f16f188b
Cynog <thomasratzketr@outlook.de>
2023-05-26 08:59:40 +0000

    parquets as single measurements

--- influxdb/import_data.py
+++ influxdb/import_data.py
@@ -2,6 +2,7 @@
 import pandas as pd
 import influxdb_client
 from influxdb_client.client.write_api import SYNCHRONOUS
+import os
 
 # influxdb config
 token = "4twszgEIHmgV0NfWQOkLHpIp-XfQoMXgiBELM5RlTWCdedWxTaX0ktqkfruSORou0s8vdYedkrNQqV2lSVqDVw=="
@@ -13,13 +14,21 @@ bucket="isd"
 client = influxdb_client.InfluxDBClient(url=url, token=token, org=org)
 write_api = client.write_api(write_options=SYNCHRONOUS)
 
-# read data into pandas df       
-df = pd.read_parquet("../data")
-df["rtctime"] = pd.to_datetime(df["rtctime"], unit="ms")
-df.set_index("rtctime", inplace=True)
 
-# write data in small chunks
-step = 10000
-for i in range(0, len(df), step):
-    print(f"\rprogress: {i/len(df): .2f}", end="")
-    write_api.write(bucket, record=df.iloc[i:i+step], data_frame_measurement_name="sensors")
+skip = 1 # use only every skip row
+chunksize = 10000 # rows of dataframes to write at once
+for fn in os.listdir("../data"):
+    # filepath
+    fp = os.path.join("../data", fn)
+    
+    # read data into pandas df       
+    df = pd.read_parquet(fp)
+    df = df.iloc[0:len(df):skip]
+    df["rtctime"] = pd.to_datetime(df["rtctime"], unit="ms")
+    df.set_index("rtctime", inplace=True)
+
+    # write data in small chunks
+    for i in range(0, len(df), chunksize):
+        write_api.write(bucket, record=df.iloc[i:i+chunksize], data_frame_measurement_name=fn)
+        print(f"\r{fn}: {i/len(df)*100: .2f}%", end="")
+    print()
