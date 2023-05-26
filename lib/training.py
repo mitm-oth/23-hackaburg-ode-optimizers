15937c75e25c34d9a9021009cf403850dffcec5e
Cynog <thomasratzketr@outlook.de>
2023-05-26 23:39:22 +0000

    starting coeff bruteforce

--- lib/training.py
+++ lib/training.py
@@ -9,9 +9,10 @@ from cutter import Cutter
 from scipy.optimize import minimize
 
 IVP_METHOD = "Radau"
+MIN_METHOD = "BFGS"
 
 
-def convolution(df, w=100):
+def convolution(df, w=20):
     conv = {}
     for col in df.columns:
         if col == "rtctime" or col == "target_temperature":
@@ -59,7 +60,7 @@ def costfun(coefficients, df, interpolations):
     solvals = sol.sol(ts)
     err = np.linalg.norm(solvals - np.array(df["target_temperature"]))
     
-    print(f"called costfun {err}: {coefficients}")
+    print(f"\rcalled costfun {err}: {coefficients}", end="")
     
     return err
 
@@ -68,16 +69,18 @@ if __name__ == "__main__":
     # get example dataframe
     cutter = Cutter("data", gap_ms=10)
     df = cutter.get_biggest_track()
-    df = df.iloc[-1100:] # cut dataframe to last 5000 rows
+    #df = df.iloc[-100000:] # cut dataframe to last 5000 rows
     print(df.head())
     print(df.shape)
     
     # specify relevant columns
-    columns = ["target_temperature", "ambient_temp", "feature_c", "feature_ct", "feature_motorspeed", "car_speed"]  #! ambient temp has to be first after target coefficient/interpolation
+    columns = ["target_temperature", "ambient_temp", "feature_c", "feature_ct", "car_speed"]  #! ambient temp has to be first after target coefficient/interpolation
     
     # convolution
     w = 20
     df = convolution(df, w)
+    df = df.iloc[10000::w//2] # skip rows for performance
+    print(df.shape)
     #df.plot(x="rtctime", y=columns)
     #plt.title("features after colvolution")
     #plt.show()
@@ -85,33 +88,36 @@ if __name__ == "__main__":
     # interpolate
     interpolations = interpolation(df, columns)
 
-    # solve ode and plot prediction
-    t_span = [df.iloc[0]["rtctime"], df.iloc[-1]["rtctime"]]
-    y0 = [df.iloc[0]["target_temperature"]]
-    coefficients = np.array([0.005, 0.0001, 0.2, 0.2, 0.2])
-    sol0 = solve_ivp(fun=odefun, t_span=t_span, y0=y0, args=(coefficients, interpolations), method=IVP_METHOD)
-    
-    #plt.plot(sol.t, sol.y.T, label="prediction")
-    #plt.plot(df["rtctime"], df["target_temperature"], label="target")
-    #plt.legend()
-    #plt.show()
-    
-    
-    
-    # optimize
-    res = minimize(costfun, coefficients, args=(df, interpolations), options={"disp": True}, callback=lambda xk: print(xk))
-    print(res)
-    
-    # solve ode and plot prediction for new coefficients
-    t_span = [df.iloc[0]["rtctime"], df.iloc[-1]["rtctime"]]
-    y0 = [df.iloc[0]["target_temperature"]]
-    sol = solve_ivp(fun=odefun, t_span=t_span, y0=y0, args=(res.x, interpolations), method=IVP_METHOD)
-    
-    plt.plot(df["rtctime"], df["target_temperature"], label="target")
-    plt.plot(df["rtctime"], df["ambient_temp"], label="ambient_temp")
-    plt.plot(df["rtctime"], df["feature_c"], label="feature_c")
-    #plt.plot(sol0.t, sol0.y.T, label="prediction init")
-    plt.plot(sol.t, sol.y.T, label="prediction")
-    plt.legend()
-    plt.savefig("prediction.pdf")
-    plt.show()
+    # try to bruteforce parameters
+    for c1 in np.logspace(10, 0.01, 3):
+        for c2 in np.logspace(10, 0.01, 3):
+            for c3 in np.logspace(10, 0.01, 4):
+                for c4 in np.logspace(10, 0.01, 4):
+                    coefficients = np.array([c1, c2,  c3, c4])
+                    print(coefficients)
+                    try:
+                        # solve ode and plot prediction
+                        t_span = [df.iloc[0]["rtctime"], df.iloc[-1]["rtctime"]]
+                        y0 = [df.iloc[0]["target_temperature"]]
+                        sol0 = solve_ivp(fun=odefun, t_span=t_span, y0=y0, args=(coefficients, interpolations), method=IVP_METHOD)
+
+                        # optimize
+                        res = minimize(costfun, coefficients, args=(df, interpolations), options={"disp": True}, method=MIN_METHOD, callback=lambda xk: print("\n\n", xk))
+                        print(res)
+
+                        # solve ode and plot prediction for new coefficients
+                        t_span = [df.iloc[0]["rtctime"], df.iloc[-1]["rtctime"]]
+                        y0 = [df.iloc[0]["target_temperature"]]
+                        sol = solve_ivp(fun=odefun, t_span=t_span, y0=y0, args=(res.x, interpolations), method=IVP_METHOD)
+
+                        plt.plot(df["rtctime"], df["target_temperature"], label="target")
+                        plt.plot(df["rtctime"], df["ambient_temp"], label="ambient_temp")
+                        plt.plot(df["rtctime"], df["feature_c"], label="feature_c")
+                        #plt.plot(sol0.t, sol0.y.T, label="prediction init")
+                        plt.plot(sol.t, sol.y.T, label="prediction")
+                        plt.title(f"{res.fun}")
+                        plt.legend()
+                        plt.savefig(f"coeffbruteforce/{c1}_{c2}_{c3}_{c4}.pdf")
+                        plt.show()
+                    except Exception as e:
+                        print(e)
