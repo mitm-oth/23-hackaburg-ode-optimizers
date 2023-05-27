de422acf8cd66058d962e8c8402a969d208e86d6
jippiee <jippiee@yahoo.com>
2023-05-27 12:22:09 +0200

    Add plots

--- tools/optimize.py
+++ tools/optimize.py
@@ -3,7 +3,105 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import integrate, interpolate, optimize, signal
 from pathlib import Path
+from collections import namedtuple
+from datetime import datetime
 import sys
+
+Measurement = namedtuple('Measurement', ['time', 'target', 'ambient', 'current', 'speed', 'cooling'])
+
+def read_measurement(path):
+    df = pd.read_parquet(path)
+    
+    time    = df["rtctime"].to_numpy()
+    target  = df["target_temperature"].to_numpy()
+    ambient = df["ambient_temp"].to_numpy()
+    current = df["feature_c"].to_numpy()
+    speed   = df["car_speed"].to_numpy()
+    cooling = df["feature_ct"].to_numpy()
+
+    # Move timestamps into nicer range
+    #time -= time[0]
+
+    return Measurement(time, target, ambient, current, speed, cooling)
+
+def running_mean(raw, window_len):
+    return signal.convolve(raw, np.ones(window_len), 'valid') / window_len
+
+def preprocess_measurement(raw, window_lens):
+    max_window_len = max(window_lens)
+    split_at = -len(raw[0]) + max_window_len
+    processed = [running_mean(data, w_len)[split_at:] for data, w_len in zip(raw, window_lens)]
+    return Measurement(*processed)
+
+def generate_heat_equation(data):
+    f_target = interpolate.interp1d(data.time, data.target)
+    f_ambient = interpolate.interp1d(data.time, data.ambient)
+    f_current = interpolate.interp1d(data.time, data.current)
+    f_speed = interpolate.interp1d(data.time, data.speed)
+    f_cooling = interpolate.interp1d(data.time, data.cooling)
+
+    def heat_transfer(t, y, c_ambient, c_current, c_speed, c_cooling):
+        return c_ambient * (f_ambient(t) - y) + c_current * f_current(t) + c_speed * f_speed(t) + c_cooling * f_cooling(t)
+
+    return heat_transfer
+
+def solve_heat_equation(heat_transfer, data, coeff):
+    #coeff = np.array([2.4e-6, 1e-8, 1.5e-6, 1e-5])
+    t_span = [data.time[0], data.time[-1]]
+    sol = integrate.solve_ivp(heat_transfer, t_span, [data.ambient[0]], method="Radau", t_eval=data.time,
+            dense_output=False, vectorized=True, args=coeff) #, rtol=1e-6, atol=1e-9)
+
+    return sol.y.T
+
+def plot_all(data, predicted, title, *, only_prediction=False, show=False, savepath=None):
+    plt.figure(figsize=(8, 6))
+    timestamps = [datetime.fromtimestamp(x / 1000.0) for x in data.time[::10]]
+    plt.plot(timestamps, data.target[::10], label="target")
+    if not only_prediction:
+        plt.plot(timestamps, data.ambient[::10], label="ambient")
+        plt.plot(timestamps, data.current[::10], label="current")
+        plt.plot(timestamps, data.speed[::10], label="speed")
+        plt.plot(timestamps, data.cooling[::10], label="cooling")
+    plt.plot(timestamps, predicted[::10], label="prediction")
+    plt.title(title)
+    #plt.legend(loc="upper right")
+    plt.xlabel("Time")
+    plt.ylabel("Temperature in degree Celcius")
+    plt.legend()
+    if savepath:
+        #plt.savefig("it_works.pdf")
+        plt.savefig(savepath)
+    if show:
+        plt.show()
+    plt.close()
+
+def plot_raw(raw, predicted):
+    #plt.plot()
+    #plt.close()
+    pass
+
+if __name__ == "__main__":
+    #path = "../data/58.parquet"
+    #path = "../data/81.parquet"
+    #path = "../data/36.parquet"
+    path = "../data/7.parquet"
+    raw = read_measurement(path)
+    window_lens = (1, 30000, 30000, 30000, 30000, 30000)
+    data = preprocess_measurement(raw, window_lens)
+    heat_transfer = generate_heat_equation(data)
+    coeff = np.array([2.4e-6, 1e-8, 1.5e-6, 1e-5])
+    predicted = solve_heat_equation(heat_transfer, data, coeff)
+    plot_all(data, predicted, "Measured data vs Predicted data", only_prediction=True, show=True,
+            savepath="../assets/7_measured_vs_predicted.pdf")
+
+sys.exit()
+
+
+###############################################################################
+###############################################################################
+###############################################################################
+
+#from dataclasses import dataclass
 
 def get_sorted_paths(root="../data", glob="*.parquet"):
     paths = list(Path(root).rglob(glob))
@@ -11,48 +109,14 @@ def get_sorted_paths(root="../data", glob="*.parquet"):
     paths.sort(key=lambda p: len(str(p)))
     return paths
 
-path = Path("../data/58.parquet")
-df = pd.read_parquet(path)
-
-#step = 1
-#n = len(df) // step
-#print("n:", n)
-
-#df = df[:n*step]
-
-#there were underscores infront here
-time = df["rtctime"].to_numpy()
-target = df["target_temperature"].to_numpy()
-ambient = df["ambient_temp"].to_numpy()
-current = df["feature_c"].to_numpy()
-speed = df["car_speed"].to_numpy()
-cooling = df["feature_ct"].to_numpy()
-
-n = len(time)
-time -= time[0]
-
-if False:
-    time = np.zeros(n)
-    target = np.zeros(n)
-    ambient = np.zeros(n)
-    current = np.zeros(n)
-    speed = np.zeros(n)
-    cooling = np.zeros(n)
-    
-    for i in range(step):
-        time += _time[i::step]
-        target += _target[i::step]
-        ambient += _ambient[i::step]
-        current += _current[i::step]
-        speed += _speed[i::step]
-        cooling += _cooling[i::step]
-    
-    time /= step
-    target /= step
-    ambient /= step
-    current /= step
-    speed /= step
-    cooling /= step
+#@dataclass
+#class Measurements:
+#    time: Any
+#    target: Any
+#    ambient: Any
+#    current: Any
+#    speed: Any
+#    cooling: Any
 
 w = 1
 #w_target  = 10000
